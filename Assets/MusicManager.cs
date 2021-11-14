7f18e57c7ac391a9e06461c29820b588717a11db
Jemora02 <jemora02@knights.ucf.edu>
2021-11-14 10:45:08 -0500

    November 14, 2021 10:45 AM

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -28,14 +28,13 @@ public class MusicManager : MonoBehaviour
         if (RubyController.scoreValue >= 4)
         {
             audioSource.Stop();
-            audioSource.Play();
             PlaySound(musicClipOne);
         }
         if (RubyController.currentHealth <= 0)
         {
             audioSource.Stop();
-            audioSource.Play();
-            PlaySound(musicClipTwo);
+            
+            Playones(musicClipTwo);
         }
     }
 }
