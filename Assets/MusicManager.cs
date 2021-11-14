295bf5c60aca282e8227d4876a587567929d1964
Jemora02 <jemora02@knights.ucf.edu>
2021-11-14 09:45:26 -0500

    November 14, 2021 9:45 AM

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -18,6 +18,21 @@ public class MusicManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Ruby Controller.scoreValue >= 4)
+        {
+            winTextObject.SetActive(true);
+            rigidbody2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = true;
+
+        }
+        if (Rubycontroller.currentHealth <= 0)
+        {
+            loseTextObject.SetActive(true);
+            rigidbody2d.constraints = RigidbodyConstraints2D.FreezeAll;
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            musicSource.loop = true;
     }
 }
